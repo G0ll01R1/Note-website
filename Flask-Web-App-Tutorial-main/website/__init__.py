f95e496b32a4eb08f744a7614de92710ad1e7818
Spinxino <32494798+Spinxino@users.noreply.github.com>
2023-05-03 02:07:55 -0700

    Update init.py
    Hello!
    It seems like Flask-SQLAlchemy 3 no longer accepts an 'app' argument to methods like 'create_all'. it requires an active Flask application context.
    
    
    def create_app():
        app = Flask(__name__)
        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///projects.db'
        db.init_app(app)
        
        from . import models
        
       with app.app_context():
             db.create_all()
       
       return app
    
    
    There is no need for the 'create_database' function. (delete it)
    Even if SQLAlchemy will not overwrite an existing file
    
    thanks, have a great day!

--- Flask-Web-App-Tutorial-main/website/__init__.py
+++ Flask-Web-App-Tutorial-main/website/__init__.py
@@ -10,11 +10,15 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///projects.db'
     db.init_app(app)
 
     from .views import views
+    from . import models
     from .auth import auth
+    
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
@@ -32,9 +36,3 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
